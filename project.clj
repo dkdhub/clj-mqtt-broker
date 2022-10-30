926da0a1475e6a5cd71a4ada7a3c37a735b31d77
MelKori <mieldo@gmail.com>
2022-10-30 20:20:29 +0200

    fix project file

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject com.dkdhub/clj-mqtt-broker "0.0.1"
   :description "A Clojure wrapper to the Moquette Broker library"
-  :url "https://github.com/source-c/clj-mqtt-broker"
-  :license {:name "Built In Project License"
-            :url  "https://github.com/source-c/clj-mqtt-broker/LICENSE"}
-  :main clj-mqtt-broker.core
+  :url "https://github.com/dkdhub/clj-mqtt-broker"
+  :license {:name "Built In Project License"}
   :dependencies [[io.moquette/moquette-broker "0.15"
                   :exclusions [com.bugsnag/bugsnag]]])
