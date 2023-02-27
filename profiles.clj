94e0fa47631a0050173468cf026b2329ff89ced4
MelKori <mieldo@gmail.com>
2023-02-27 13:35:08 +0200

    update profiles

--- profiles.clj
+++ profiles.clj
@@ -5,13 +5,13 @@
 
             :dependencies [[ch.qos.logback/logback-classic "1.4.5"
                             :exclusions [org.slf4j/slf4j-api]]
-                           [org.slf4j/jul-to-slf4j "2.0.5"]
-                           [org.slf4j/jcl-over-slf4j "2.0.5"]
-                           [org.slf4j/log4j-over-slf4j "2.0.5"]
+                           [org.slf4j/jul-to-slf4j "2.0.6"]
+                           [org.slf4j/jcl-over-slf4j "2.0.6"]
+                           [org.slf4j/log4j-over-slf4j "2.0.6"]
                            [org.clojure/tools.logging "1.2.4"]]}
 
  :provided {:dependencies      [[org.clojure/clojure "1.11.1"]
-                                [org.slf4j/log4j-over-slf4j "2.0.5"]]
+                                [org.slf4j/log4j-over-slf4j "2.0.6"]]
             :source-paths      #{"src-clj"}
             :java-source-paths #{"src-java"}
             :resource-paths    ["resources"]
