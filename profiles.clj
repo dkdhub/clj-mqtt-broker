5ceafefb006b13bf829d409cce1ae3a794ff82b5
MelKori <mieldo@gmail.com>
2023-04-27 21:39:00 +0300

    upgrade logging libraries

--- profiles.clj
+++ profiles.clj
@@ -3,15 +3,15 @@
             :target-path   "target/%s"
             :clean-targets ^{:protect false} [:target-path]
 
-            :dependencies [[ch.qos.logback/logback-classic "1.4.5"
+            :dependencies [[ch.qos.logback/logback-classic "1.4.7"
                             :exclusions [org.slf4j/slf4j-api]]
-                           [org.slf4j/jul-to-slf4j "2.0.6"]
-                           [org.slf4j/jcl-over-slf4j "2.0.6"]
-                           [org.slf4j/log4j-over-slf4j "2.0.6"]
+                           [org.slf4j/jul-to-slf4j "2.0.7"]
+                           [org.slf4j/jcl-over-slf4j "2.0.7"]
+                           [org.slf4j/log4j-over-slf4j "2.0.7"]
                            [org.clojure/tools.logging "1.2.4"]]}
 
  :provided {:dependencies      [[org.clojure/clojure "1.11.1"]
-                                [org.slf4j/log4j-over-slf4j "2.0.6"]]
+                                [org.slf4j/log4j-over-slf4j "2.0.7"]]
             :source-paths      #{"src-clj"}
             :java-source-paths #{"src-java"}
             :resource-paths    ["resources"]
