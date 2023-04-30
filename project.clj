8a21bc92114f8ea2940d1a522c7dade3a4bffaa4
MelKori <mieldo@gmail.com>
2023-05-01 02:53:46 +0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.dkdhub/clj-mqtt-broker "0.0.5"
+(defproject com.dkdhub/clj-mqtt-broker "0.0.6"
   :description "A Clojure wrapper to the Moquette Broker library"
   :url "https://github.com/dkdhub/clj-mqtt-broker"
   :license {:name "Built In Project License"}
