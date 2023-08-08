12d5e0073b43bfd825f4ac9888e48ee94dbf83ef
MelKori <mieldo@gmail.com>
2023-08-08 04:27:11 +0300

    release with moquette 0.17

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.dkdhub/clj-mqtt-broker "0.0.6-SNAPSHOT"
+(defproject com.dkdhub/clj-mqtt-broker "0.0.6"
   :description "A Clojure wrapper to the Moquette Broker library"
   :url "https://github.com/dkdhub/clj-mqtt-broker"
   :license {:name "Built In Project License"}
-  :dependencies [[io.moquette/moquette-broker "0.17-SNAPSHOT"
+  :dependencies [[io.moquette/moquette-broker "0.17"
                   :exclusions [com.bugsnag/bugsnag
                                org.slf4j/slf4j-api
                                org.slf4j/slf4j-log4j12]]])
